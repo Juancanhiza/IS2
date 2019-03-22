c63cd36e9a183f316bc064bbd7c560b6b6c83cec
Maria Jose Grance Avila <majo.grance014@gmail.com>
2019-03-22 01:24:48 -0300

    se agrega el template para inicio de sesion

---
+++ poliproyecto/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+"""
+Vista del Login
+"""
+
+def home(request):
+    """
+    Vista de la pagina de inicio
+    :param request:
+    """
+    return render(request, '../templates/poliproyecto/index.html')
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
 
-
 class MyTestCase(unittest.TestCase):
+    '''Esta clase fue creada para probar'''
     def test_something(self):
         self.assertEqual(True, False)
 
 
-if __name__ == '__main__':
+if __name__ == '__pip main__':
     unittest.main()
