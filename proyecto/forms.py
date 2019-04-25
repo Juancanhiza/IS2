7ae6c7eb3b29b288ab3949a505e597a86793f6c4
Maria Jose Grance Avila <majo.grance014@gmail.com>
2019-04-24 23:27:12 -0400

    asignacion de roles

--- proyecto/forms.py
+++ proyecto/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-
-from .models import Proyecto
+from django.forms import inlineformset_factory
+from .models import *
 
 class CreateProjectForm(forms.ModelForm):
     fecha_inicio = forms.DateTimeField()
@@ -10,9 +10,17 @@ class CreateProjectForm(forms.ModelForm):
         fields = ('nombre', 'fecha_inicio', 'fecha_fin', 'estado', 'descripcion')
 
 class UpdateProjectForm(forms.ModelForm):
-    fecha_inicio = forms.DateTimeField()
-    fecha_fin = forms.DateTimeField()
     class Meta:
         model = Proyecto
         fields = ('nombre', 'fecha_inicio', 'fecha_fin', 'estado', 'descripcion')
+
+class ProyectoDetalleForm(forms.ModelForm):
+    class Meta:
+        model = ProyectoDetalle
+        exclude = ()
+        fields = ('usuario','rol')
+
+ProyectoDetalleFormSet = inlineformset_factory(Proyecto,ProyectoDetalle,form=ProyectoDetalleForm,extra=1)
+
+
 
