84178790db2c33b4ccc6c62ac4254415467d2f57
Juancanhiza <juancahiza@gmail.com>
2019-04-27 04:51:15 -0400

    Otros tests

--- userstory/tests.py
+++ userstory/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import unittest
+from userstory.models import UserStory
+from proyecto.models import Proyecto
 
-# Create your tests here.
+class Test(unittest.TestCase):
+    """
+    Test para creación de US
+    """
+    def test_creacionUs(self):
+        us = UserStory()
+        self.assertIsNotNone(us)
+    """
+    Test de Rango permitido
+    """
+    def test_Rango(self):
+        proyecto1 = Proyecto(nombre='Proyecto1', fecha_inicio='23/04/2019', fecha_fin='22/04/2019')
+        us = UserStory(proyecto=proyecto1, nombre="p1", descripcion="desc", fecha_inicio='13/04/2019', duracion_estimada=100, valor_negocio=55)
+        self.assertLessEqual(us.valor_negocio, 10)
