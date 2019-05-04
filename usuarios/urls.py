f254b05613b00c2ba92f441ba726764223b0c5aa
Maria Jose Grance Avila <majo.grance014@gmail.com>
2019-05-04 09:56:42 -0400

    correccion de guardado de usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.UserListView.as_view(),name='user_list'),
 	url(r'^create/$', views.CreateUserView.as_view(), name='create_user'),
-	path(route='<int:pk>/', view=views.UpdateUserView.as_view(), name='update_user'),
+	path(route='modificar/<int:pk>/', view=views.UpdateUserView.as_view(), name='update_user'),
+	path(route='ver/<int:pk>/', view=views.VerUserDetailView.as_view(), name='ver_user')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Usuario
 from .forms import *
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -53,3 +51,17 @@ class UpdateUserView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
 
     def get_absolute_url(self):
         return reverse('update_user', kwargs={'pk': self.kwargs['pk']})
+
+
+@method_decorator(login_required, name='dispatch')
+class VerUserDetailView(LoginRequiredMixin, SuccessMessageMixin, DetailView):
+    model = Usuario
+    template_name = 'usuarios/ver_user.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Ver Usuario"
+        return context
+
+    def get_object(self, queryset=None):
+        return Usuario.objects.get(pk=self.kwargs['pk'])
